730a939608af9bd11697914cce6d31c556df39b5
h1ddengames <skarim3000@gmail.com>
2020-11-12 23:17:17 -0800

    Added project template

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Epic("Arithmetic Operations")
+@Feature("Addition and Subtraction Features")
+public class AppTest {
+    private final App app = new App();
+
+    @Test
+    @Story("User tries to add two numbers.")
+    @Description("Get the value of two numbers added together.")
+    void addition() {
+        assertEquals(2, app.add(1,1));
+    }
+
+    @Test
+    @Story("User tries to subtract two numbers.")
+    @Description("Get the value of a number subtracted by another number.")
+    void subtraction() {
+        assertEquals(2, app.subtract(4, 2));
+    }
+
+    @Test
+    @Story("User tries to add two numbers then subtract from a larger number.")
+    @Description("Get the value of a number subtracted by two numbers added together.")
+    void integrationOfAdditionAndSubtraction() {
+        assertEquals(5, app.subtract(10, app.add(4, 1)));
+    }
+}
