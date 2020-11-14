70a6b9cd4cf770dc6c67cf23cac2cbd5da68d0ac
h1ddengames <skarim3000@gmail.com>
2020-11-13 23:49:06 -0800

    Added API testing, added allure properties
    The allure.properties file sets the report generation directory to target/allure-results rather than having allure-results in the project's base directory.

---
+++ src/main/java/DogAPI.java
@@ -0,0 +1,40 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class DogAPI {
+    public static Response getAllDogBreeds() {
+        return given()
+                .config(RestManager.getConfig())
+                //.header("Authorization", "Bearer " + bearerToken)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                //.body()
+                .baseUri("https://dog.ceo")
+                .basePath("/api/breeds/list/all")
+                .get();
+    }
+
+    public static Response getRandomDogImage() {
+        return given()
+                .config(RestManager.getConfig())
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .baseUri("https://dog.ceo")
+                .basePath("/api/breeds/image/random")
+                .get();
+    }
+
+    public static Response getMultipleRandomDogImages(int amount) {
+        if(amount > 50) { return null; }
+
+        return given()
+                .config(RestManager.getConfig())
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .baseUri("https://dog.ceo")
+                .basePath("/api/breeds/image/random/" + amount)
+                .get();
+    }
+}
---
+++ src/main/java/RestManager.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
+import org.apache.http.params.CoreConnectionPNames;
+
+public class RestManager {
+    public static RestAssuredConfig getConfig() {
+        // Timeout is in milliseconds.
+        // CONNECTION_TIMEOUT is 'until a connection is established'.
+        // SO_TIMEOUT is 'a maximum period inactivity between two consecutive data packets'.
+        return RestAssured.config()
+                .httpClient(HttpClientConfig.httpClientConfig()
+                        .setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 5000)
+                        .setParam(CoreConnectionPNames.SO_TIMEOUT, 5000));
+    }
+}
