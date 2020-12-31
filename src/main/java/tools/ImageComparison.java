36d9c1508dd539935168d21fe9cda44d31b6d50c
h1ddengames <skarim3000@gmail.com>
2020-12-31 15:34:17 -0800

    Added annotations and comments to ImageComparison

--- src/main/java/tools/ImageComparison.java
+++ src/main/java/tools/ImageComparison.java
@@ -1,8 +1,20 @@
 package tools;
 
+import annotations.API;
+import annotations.ClassMetadata;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+/**
+ * Compares two images and generates an image highlighting the differences.
+ */
+@ClassMetadata(
+        author = "Shahid Karim", dateCreated = "12/29/2020",
+        currentRevision = 2, lastModified = "12/30/2020", lastModifiedBy = "Shahid Karim",
+        reviewers = {}
+)
+@API(status = API.Status.STABLE, since = "Project-Template-v2.0.0", consumers = {})
 public class ImageComparison {
     final static int highlight = new Color(120, 0, 150, 255).getRGB();
     final static int transparent = new Color(0, 0, 0, 0).getRGB();
@@ -17,15 +29,26 @@ public class ImageComparison {
         HIGHLIGHT, BLACK, WHITE, TRANSPARENT, NONE
     }
 
-    // Usage:
-    // ImageIO.write(ImageComparison.getDifferenceBetweenImages(
-    //                        ImageIO.read(new File("src/main/resources/expected-image.png")),
-    //                        ImageIO.read(new File("src/main/resources/actual-image.png")),
-    //                        ImageComparison.OnDifferenceOption.ADD_HIGHLIGHT,
-    //                        ImageComparison.OnSimilarOption.NONE,
-    //                        ),
-    //                "png",
-    //                new File("src/main/resources/differences.png"));
+    /**
+     * Compares two images pixel by pixel for differences then based on input will generate a new image highlighting the differences.
+     * Usage:
+     * <pre>{@code
+     *      ImageIO.write(ImageComparison.getDifferenceBetweenImages(
+     *                             ImageIO.read(new File("src/main/resources/expected-image.png")),
+     *                             ImageIO.read(new File("src/main/resources/actual-image.png")),
+     *                             ImageComparison.OnDifferenceOption.ADD_HIGHLIGHT,
+     *                             ImageComparison.OnSimilarOption.NONE,
+     *                             ),
+     *                     "png",
+     *                     new File("src/main/resources/differences.png"));
+     * }</pre>
+     * @param image1 The expected image.
+     * @param image2 The actual image.
+     * @param differenceOption What should be done when a pixel is different?
+     * @param similarOption What should be done when a pixel is similar?
+     * @return A BufferedImage to be saved to file.
+     */
+    @API(status = API.Status.STABLE, since = "Project-Template-v2.0.0", consumers = {})
     public static BufferedImage getDifferenceBetweenImages(BufferedImage image1, BufferedImage image2,
                                                            OnDifferenceOption differenceOption, OnSimilarOption similarOption) {
         // Convert images to pixel arrays.
