fcfc684103dc270d7f3e9171de912a5f5cd9a401
h1ddengames <skarim3000@gmail.com>
2021-01-07 16:37:01 -0800

    Fixed failing test-workflow.yml
    Added better comments explaining how maven exec plugin works in pom.xml
    Added a debugging get-working-directory.sh script.
    Updated previous scripts to have better formatting.

--- scripts/generate-reports.sh
+++ scripts/generate-reports.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../.allure/allure-2.8.1/bin/allure generate docs/allure-results --clean -o docs/allure-reports && echo "GENERATED REPORT"
+.allure/allure-2.8.1/bin/allure generate docs/allure-results --clean -o docs/allure-reports && echo "[INFO] GENERATED ALLURE REPORT."
---
+++ scripts/get-working-directory.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "[INFO] CURRENT WORKING DIRECTORY: $PWD"
