3392259c8fffb8d5f404e249b4ce90e0318b986d
h1ddengames <skarim3000@gmail.com>
2021-02-13 17:29:40 -0800

    Added ReverseString practice from exercism

---
+++ src/main/java/site/hiddengames/practice/exercism/reversestring/ReverseString.java
@@ -0,0 +1,27 @@
+package site.hiddengames.practice.exercism.reversestring;
+
+import site.hiddengames.annotations.API;
+import site.hiddengames.annotations.ClassMetadata;
+
+/**
+ * Reverse a string.
+ */
+@ClassMetadata(
+        author = "Shahid Karim", dateCreated = "2/13/2021",
+        currentRevision = 1, lastModified = "2/13/2021", lastModifiedBy = "Shahid Karim",
+        reviewers = {}
+)
+@API(status = API.Status.STABLE, since = "Project-Template-v3.0.0", consumers = { "ReverseStringTest" })
+public class ReverseString {
+    String reverse(String inputString) {
+        if(inputString == null || inputString.contentEquals("")) { return ""; }
+
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversedString.append(inputString.charAt(i));
+        }
+
+        return reversedString.toString();
+    }
+}
---
+++ src/test/java/site/hiddengames/practice/exercism/reversestring/ReverseStringTest.java
@@ -0,0 +1,38 @@
+package site.hiddengames.practice.exercism.reversestring;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ReverseStringTest {
+
+    @Test
+    public void testAnEmptyString() {
+        assertEquals("", new ReverseString().reverse(""));
+    }
+
+    @Test
+    public void testAWord() {
+        assertEquals("tobor", new ReverseString().reverse("robot"));
+    }
+
+    @Test
+    public void testACapitalizedWord() {
+        assertEquals("nemaR", new ReverseString().reverse("Ramen"));
+    }
+
+    @Test
+    public void testASentenceWithPunctuation() {
+        assertEquals("!yrgnuh m'I", new ReverseString().reverse("I'm hungry!"));
+    }
+
+    @Test
+    public void testAPalindrome() {
+        assertEquals("racecar", new ReverseString().reverse("racecar"));
+    }
+
+    @Test
+    public void testAnEvenSizedWord() {
+        assertEquals("reward", new ReverseString().reverse("drawer"));
+    }
+}
